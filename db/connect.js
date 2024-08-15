df0d14a82ffad377e40cd04458d9ae5c9ad1b9ad
Arnob Pandit <142673119+project-file-arnob-pandit@users.noreply.github.com>
2024-08-15 14:20:51 +0530

    Update connect.js
    mongoDB url hide

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://arnobpandit2033:simple@ac-dkdp5my-shard-00-00.xg38c3c.mongodb.net:27017,ac-dkdp5my-shard-00-01.xg38c3c.mongodb.net:27017,ac-dkdp5my-shard-00-02.xg38c3c.mongodb.net:27017/Data?ssl=true&replicaSet=atlas-62qt3z-shard-0&authSource=admin&retryWrites=true&w=majority&appName=project")
+        await mongoose.connect("")
         console.log("db connect sucessfully");
     } catch (error) {
         console.log("db connect failed !!");
