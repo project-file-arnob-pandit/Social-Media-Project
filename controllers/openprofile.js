5bc48c532b023f4a208446d78399ac316cf41331
Arnob Pandit <142673119+project-file-arnob-pandit@users.noreply.github.com>
2024-08-18 15:51:22 +0530

    Update openprofile.js
    mutual friends add logic

--- controllers/openprofile.js
+++ controllers/openprofile.js
@@ -16,18 +16,33 @@ export const openprofile = async (req, res) => {
         if (!user) {
             return res.redirect("/login"); // Redirect to login if profile user is not found
         }
+
         // Fetch the current user's friends based on populated friends field
         const currentUserFriends = await User.find({ _id: { $in: currentUser.friends } });
+
         // Fetch the profile user's friends based on populated friends field
-        const friends = await User.find({ _id: { $in: user.friends } });
+        const profileUserFriends = await User.find({ _id: { $in: user.friends } });
+
+        // Determine mutual friends
+        const mutualFriends = currentUserFriends.filter(friend =>
+            profileUserFriends.some(profileFriend => profileFriend._id.equals(friend._id))
+        );
+
         // Fetch posts made by the profile user
         const userPosts = await Post.find({ user: id });
-        // Filter out the current user from all users array
-        const imageUrls = userPosts.map(post => post.image);
-        // Render the view with fetched data
-        
 
-        res.render("openprofile", { user, imageUrls, friends, currentUser, currentUserFriends });
+        // Map over user posts to get the image URLs
+        const imageUrls = userPosts.map(post => post.image);
+
+        // Render the view with fetched data including mutual friends
+        res.render("openprofile", {
+            user,
+            imageUrls,
+            friends: profileUserFriends,
+            currentUser,
+            currentUserFriends,
+            mutualFriends  // Pass mutual friends to the view
+        });
     } catch (error) {
         console.error(error); // Log the error for debugging purposes
         res.status(500).send("Internal Server Error");
